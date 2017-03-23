3b7318ac5491ce06c617b658d50b45ea432f8e67
BenBoonya <benbenn223@gmail.com>
2017-03-23 17:34:44 +0700

    update gradle and libs

--- app/src/main/java/com/boonya/ben/ldproject/MainActivity.java
+++ app/src/main/java/com/boonya/ben/ldproject/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements SpeakClick {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (android.os.Build.VERSION.SDK_INT > 9) {
+        if (android.os.Build.VERSION.SDK_INT >= 14) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
