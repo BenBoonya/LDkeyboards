8182afc789124242d1a3cc0da7271b430ac6a161
BenBoonya <benbenn223@gmail.com>
2017-03-26 09:38:36 +0700

    delete unused code

--- app/src/main/java/com/boonya/ben/ldproject/adapter/ListViewAdapter.java
+++ app/src/main/java/com/boonya/ben/ldproject/adapter/ListViewAdapter.java
@@ -18,7 +18,7 @@ import com.boonya.ben.ldproject.model.Best_Spell_Model;
 import java.util.ArrayList;
 
 public class ListViewAdapter extends BaseAdapter {
-    private static final String TAG = ListViewAdapter.class.getSimpleName() ;
+    private static final String TAG = ListViewAdapter.class.getSimpleName();
     private Context context;
     private ArrayList<Best_Spell_Model> contactModels;
     private boolean[] checkedStates;
@@ -68,7 +68,6 @@ public class ListViewAdapter extends BaseAdapter {
             viewPosition = new MainActivity().getCurrentPos();
 
             holder.txtTitle = (TextView) convertView.findViewById(R.id.txtTitle);
-           /* holder.txtSize = (TextView) convertView.findViewById(R.id.txtSize);*/
             holder.thumbImage = (ImageView) convertView.findViewById(R.id.listen);
             holder.thumbImage.setImageResource(R.drawable.book_close_icon);
 
@@ -79,37 +78,27 @@ public class ListViewAdapter extends BaseAdapter {
         }
         final Best_Spell_Model model = contactModels.get(position);
 
-       Log.i("ViewPosition", ""+viewPosition);
+        Log.i("ViewPosition", "" + viewPosition);
 
-        if(position != viewPosition){
+        if (position != viewPosition) {
             holder.thumbImage.setImageResource(R.drawable.book_close_icon);
         }
 
-
-//        else if(position == viewPosition ){
-//            holder.thumbImage.setImageResource(R.drawable.book_icon);
-//        }
-
         holder.txtTitle.setText(model.getmSenseGroup());
-//        holder.txtSize.setText(model.getmG2P());
-        if ((model.getmDescription() != null) && (model.getmDescription().trim().length() >1) ) {
-            //Log.i(TAG, model.getmSenseGroup()+"  :: is null or length is zero" + model.getmDescription() );
+        if ((model.getmDescription() != null) && (model.getmDescription().trim().length() > 1)) {
             holder.thumbImage.setVisibility(View.VISIBLE);
         }
 
-        if ((model.getmDescription() == null || model.getmDescription().trim().length() == 0) ) {
-            Log.i(TAG, model.getmSenseGroup()+"  :: is null or length is zero" + model.getmDescription() );
+        if ((model.getmDescription() == null || model.getmDescription().trim().length() == 0)) {
+            Log.i(TAG, model.getmSenseGroup() + "  :: is null or length is zero" + model.getmDescription());
             holder.thumbImage.setVisibility(View.INVISIBLE);
-       }
-        else if ((model.getmDescription() != null) && (model.getmDescription().trim().length() == 1) ) {
-            Log.i(TAG, "model.getmDescription() is ::  "+model.getmDescription());
+        } else if ((model.getmDescription() != null) && (model.getmDescription().trim().length() == 1)) {
+            Log.i(TAG, "model.getmDescription() is ::  " + model.getmDescription());
             if (model.getmDescription().equalsIgnoreCase("-")) {
                 holder.thumbImage.setVisibility(View.INVISIBLE);
             }
         }
         holder.txtTitle.setTag(model.getmSenseGroup());
-
-
 
 
         holder.txtTitle.setOnClickListener(new View.OnClickListener() {
@@ -123,7 +112,7 @@ public class ListViewAdapter extends BaseAdapter {
                     if (MainActivity.tts != null) {
                         Log.i("tts status :: ", "tts is not null *** ***");
                         MainActivity.tts.speak(model.getmSenseGroup(), TextToSpeech.QUEUE_FLUSH, null);
-                       // Toast.makeText(context, model.getmSenseGroup(), Toast.LENGTH_LONG).show();
+                        // Toast.makeText(context, model.getmSenseGroup(), Toast.LENGTH_LONG).show();
                     } else {
                         Log.i("tts status :: ", "tts is null *** ***");
                         speak.speak(model.getmDescription());
@@ -141,74 +130,14 @@ public class ListViewAdapter extends BaseAdapter {
         holder.thumbImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                if (model.getmDescription() != null) {
-//                    MainActivity.mTts.speak(model.getmDescription(), TextToSpeech.QUEUE_FLUSH, null);
-//                } else {
-//                    MainActivity.mTts.speak("No Description is available." + model.getmSenseGroup(), TextToSpeech.QUEUE_FLUSH, null);
-//                }
-
-
                 if (model.getmDescription() != null) {
-//                    checkTTSEngineInstalled(SVOX_TTS_ENGINE);
-//                    if (tts != null) {
-//                        Log.i("tts status :: ", "tts is not null *** ***");
-//                        ApplicationClass.tts.speak(model.getmDescription(), TextToSpeech.QUEUE_FLUSH, null);
-//                        Toast.makeText(context, model.getmDescription(), Toast.LENGTH_LONG).show();
-//                    } else {
                     Log.i("tts status :: ", "tts is null *** ***");
-                   // speak.speak(model.getmDescription());
-                    MainActivity.tts.speak(model.getmDescription(), TextToSpeech.QUEUE_FLUSH,null);
-//                    }
-
-//                    MainActivity.mTts.speak(model.getmDescription(), TextToSpeech.QUEUE_FLUSH, null);
+                    MainActivity.tts.speak(model.getmDescription(), TextToSpeech.QUEUE_FLUSH, null);
                 } else {
-//                    ApplicationClass.tts.speak("No Description is available." + model.getmSenseGroup(), TextToSpeech.QUEUE_FLUSH, null);
-                    MainActivity.tts.speak("No description", TextToSpeech.QUEUE_FLUSH,null);
+                    MainActivity.tts.speak("No description", TextToSpeech.QUEUE_FLUSH, null);
                 }
             }
         });
-
-        // String[] proj = { MediaStore.Video.Media._ID,
-        // MediaStore.Video.Media.DISPLAY_NAME,
-        // MediaStore.Video.Media.DATA };
-        // Cursor cursor = context.getContentResolver().query(
-        // MediaStore.Video.Media.EXTERNAL_CONTENT_URI, proj,
-        // MediaStore.Video.Media.DISPLAY_NAME + "=?",
-        // new String[] { model.getName() }, null);
-        // cursor.moveToFirst();
-        // long ids = cursor.getLong(cursor
-        // .getColumnIndex(MediaStore.Video.Media._ID));
-
-
-        // if (isScrollStop) {
-        // ContentResolver crThumb = context.getContentResolver();
-        // BitmapFactory.Options options = new BitmapFactory.Options();
-        // options.inSampleSize = 1;
-        // Bitmap curThumb = MediaStore.Video.Thumbnails.getThumbnail(crThumb,
-        // model.getVideoId(), MediaStore.Video.Thumbnails.MICRO_KIND,
-        // options);
-        // holder.thumbImage.setImageBitmap(curThumb);
-        // curThumb = null;
-        // }
-        // holder.ll.setOnClickListener(new OnClickListener() {
-        //
-        // @Override
-        // public void onClick(View arg0) {
-        // // TODO Auto-generated method stub
-        // //playVideo(model.getVideo_url());
-        // }
-        //
-        // });
-        // holder.thumbImage.setOnClickListener(new OnClickListener() {
-        //
-        // @Override
-        // public void onClick(View arg0) {
-        // // TODO Auto-generated method stub
-        // //playVideo(model.getVideo_url());
-        // }
-        // });
-//        convertView.setTag(model.getmSenseGroup());
-//convertView.set
         return convertView;
     }
 
@@ -222,34 +151,8 @@ public class ListViewAdapter extends BaseAdapter {
         return countTrue;
     }
 
-//    public ArrayList<ContactsModel> getSelectedVideos() {
-//        int count = getSelectedCount();
-//        System.out.println("Count ******** " + count);
-//        ArrayList<ContactsModel> selectedModels = new ArrayList<ContactsModel>();
-//        int selectedItemsCount = 0;
-//        for (ContactsModel d : contactModels) {
-//            if (d.isSelected()) {
-//                selectedItemsCount++;
-//                selectedModels.add(d);
-//                if (count == selectedItemsCount) {
-//                    break;
-//                }
-//            }
-//            // something here
-//        }
-//        return selectedModels;
-//    }
-
-	/*
-     * private void playVideo(String videoUrl) { // TODO Auto-generated method
-	 * stub Intent intent = new Intent(context, ViewVideo.class);
-	 * intent.putExtra("videofilename", videoUrl);
-	 * context.startActivity(intent); }
-	 */
-
     static class ViewHolder {
         TextView txtTitle;
-        //        TextView txtSize;
         ImageView thumbImage;
     }
 }
